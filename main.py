7391b96589a9907f00107c9abcc3867f40cad71f
MrTeacheer <bekturmoidinov@gmail.com>
2024-08-24 22:59:12 -0700

    dz2

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from aiogram import executor
 from config import bot,dp
 from handlers import (start,
                       echo,
-                      commands)
+                      commands,
+                      quiz,
+                      game)
 
 
 start.register_start(dp=dp)
 commands.register_commands(dp=dp)
+quiz.register_quiz(dp=dp)
+game.register_game(dp=dp)
 echo.register_echo(dp=dp)
 
 
